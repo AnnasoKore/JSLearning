3f70cc16b3771a729b6ddfdf669236b78995b1fa
AnnasoKore <annasokore220@gmail.com>
2023-07-21 10:23:16 +0530

    First JavaScript Hello World program
    First JavaScript Hello World program

--- helloWorld.js
+++ helloWorld.js
@@ -1,2 +1,13 @@
 console.log("Hello World");
 console.log("My name is Annaso Kore");
+
+var num1=100;
+var num2=200;
+console.log("Before Swap")
+console.log("num1 => ",num1 , "num2 => ", num2);
+var temp=num1;
+num1=num2;
+num2=temp;
+console.log("After Swap")
+console.log("num1 => ",num1 , "num2 => ", num2);
+
