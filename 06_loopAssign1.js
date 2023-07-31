d5074c4ffcd64b7734030c60196095229d2e684c
AnnasoKore <annasokore220@gmail.com>
2023-07-31 09:50:29 +0530

    Assignment on loops
    Assignment on for loop and while loop

---
+++ 06_loopAssign1.js
@@ -0,0 +1,75 @@
+console.log("-: For Loop :-");
+console.log("--------------------- Task - 1 --------------------");
+for (let index = 5; index <= 15; index++) {
+    console.log(index);
+}
+console.log("--------------------- Task - 2 --------------------");
+for (let index = 50; index >=40; index--) {
+    console.log(index);
+    
+}
+console.log("--------------------- Task - 3 --------------------");
+for (let index = 1; index < 30; index+=2) {
+   console.log(index);    
+}
+console.log("--------------------- Task - 4 --------------------");
+for (let index = 0; index <=18; index+=2) {
+   console.log(index);
+}
+console.log("--------------------- Task - 5 --------------------");
+var str="  ";
+for (let index = 5; index <=50; index+=5) {
+   console.log(`${index}`)
+}
+console.log("--------------------- Task - 6 --------------------");
+for (let index = 10; index <=100; index+=10) {
+    console.log(index);
+}
+console.log("--------------------- Task - 7 --------------------");
+for (let index = 100; index >=10; index-=10) {
+    console.log(index);
+}
+
+console.log('-: While Loop :-');
+console.log("--------------------- Task - 1 --------------------");
+var i=5;
+while (i<=15) {
+    console.log(i);
+    i++;
+}
+console.log("--------------------- Task - 2 --------------------");
+var a=50;
+while (a>=40) {
+    console.log(a);
+    a--;
+}
+console.log("--------------------- Task - 3 --------------------");
+var b=1;
+while (b<30) {
+    console.log(b);
+    b+=2;
+}
+console.log("--------------------- Task - 4 --------------------");
+var c=0;
+while (c<=18) {
+    console.log(c);
+    c+=2;
+}
+console.log("--------------------- Task - 5 --------------------");
+var d=5;
+while (d<=50) {
+    console.log(d);
+    d+=5;
+}
+console.log("--------------------- Task - 6 --------------------");
+var e=10;
+while (e<=100) {
+    console.log(e);
+    e+=10;
+}
+console.log("--------------------- Task - 7 --------------------");
+var f=100;
+while (f>=10) {
+    console.log(f);
+    f-=10;
+}
